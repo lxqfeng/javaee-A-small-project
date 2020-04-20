04bd7ad13406f70df28838635a9eb12a2928715c
lxqfeng <lxqfeng619@163.com>
2020-04-20 23:45:09 +0800

    change

--- hjj/src/test/test.java
+++ hjj/src/test/test.java
@@ -4,5 +4,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("Hello everybody!");
 		System.out.println("This is a commit change.");
+		System.out.println("This is only a change.");
 	}
 }
